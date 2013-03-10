c884ddf278248da21bea46892561222958edc005
Julien Delafontaine <julien.delafontaine@epfl.ch>
2013-03-10 21:28:20 +0100

    densities

--- gless.py
+++ gless.py
@@ -10,7 +10,7 @@ class Parse:
        bed and bedGraph formats only."""
     def __init__(self,filename):
         self.filehandle = os.path.abspath(filename)
-        self.format = os.path.splitext(filename)[1]
+        self.format = os.path.splitext(filename)[1][1:]
     def read(self,chrom=None,fields=None):
         with open(self.filehandle) as f:
             for line in f:
@@ -31,11 +31,19 @@ try:
 except ImportError:
     track = Parse
 
+def format(filename):
+    """Return whether it is a track with 'intervals' or a 'density'."""
+    with track(filename) as t:
+        if t.format.lower() in ['bed']:
+            return 'intervals'
+        elif t.format.lower() in ['bedgraph','wig','bigWig','sga']:
+            return 'density'
+
 def read10(trackList,chr,nfeat,nbp,selection):
-    """Yield a list of lists [[(1,2),(3,4)], [(1,3),(5,6)]] with either the *nfeat*
-       next items, or all next items within an *nbp* window."""
-    fields = ['start','end']
-    streams = [track(t).read(chr,fields=fields) for t in trackList]
+    """Yield a list of lists [[(1,2,n),(3,4,n)], [(1,3,n),(5,6),n]] with either the *nfeat*
+       next items, or all next items within an *nbp* window. `n` is a name or a score."""
+    tracks = [track(t) for t in trackList]
+    streams = [t.read(chr,fields=t.fields[1:4]) for t in tracks]
     available_streams = range(len(streams))
     if nbp: nfeat = None
     elif nfeat: nbp = None
@@ -61,8 +69,8 @@ def read10(trackList,chr,nfeat,nbp,selection):
                 maxpos = max(x[-1][1] for x in toyield if x)
                 for n,x in enumerate(current):
                     if x[0][0] < maxpos:
-                        toyield[x[-1]].append((x[0][0],min(x[0][1],maxpos)))
-                        current[n][0] = (min(x[0][1],maxpos),x[0][1])
+                        toyield[x[-1]].append((x[0][0],min(x[0][1],maxpos),x[0][2]))
+                        current[n][0] = (min(x[0][1],maxpos),x[0][1],x[0][2])
                 yield toyield
             else: break
     elif nbp:
@@ -79,25 +87,33 @@ def read10(trackList,chr,nfeat,nbp,selection):
                 while current[n]:
                     x = current[n]
                     if x[1] <= k*nbp:
-                        toyield[n].append((x[0]-(k-1)*nbp,x[1]-(k-1)*nbp))
+                        toyield[n].append((x[0]-(k-1)*nbp,x[1]-(k-1)*nbp,x[2]))
                         try: current[n] = streams[n].next()
                         except StopIteration:
                             current[n] = None
                             toremove.append(n)
                     elif x[0] < k*nbp:
-                        toyield[n].append((x[0]-(k-1)*nbp,nbp))
-                        current[n] = (k*nbp,x[1])
+                        toyield[n].append((x[0]-(k-1)*nbp,nbp,x[2]))
+                        current[n] = (k*nbp,x[1],x[2])
                     else: break
             for n in toremove: available_streams.remove(n)
             if any(toyield):
                 yield toyield
             else:
-                yield [[(0,0)] for _ in streams]
+                yield [[(0,0,'')] for _ in streams]
 
 def draw(names,tracks_content,geometry,nfeat,nbp,ntimes,maxpos):
     """Create a new window and draw from the *tracks_content* coordinates
-       (of the form [[(1,2),(3,4)], [(3,5),(4,6)],...] )."""
-    WIN_WIDTH=700
+       (of the form [[(1,2,n),(3,4,n)], [(3,5,n),(4,6,n)],...],
+       where `n` is either a name or a score)."""
+    WIN_WIDTH=800
+    htrack = 30
+    rmargin = 100
+    feat_pad = 10
+    bg = "grey"
+    canvas_bg = "white"
+    line_col = "black"
+    feat_col = "blue"
     def _bp2px(y,wwidth,reg_bp):
         try: return y*wwidth/reg_bp
         except ZeroDivisionError: return 0
@@ -112,77 +128,76 @@ def draw(names,tracks_content,geometry,nfeat,nbp,ntimes,maxpos):
             pass
     root = tk.Tk()
     root.bind("<Key>", exit_on_Esc)
-    root.config(bg="grey")
+    root.config(bg=bg)
     if geometry: # keep previous position of the window
         geometry = '+'.join(['']+geometry.split('+')[1:]) # "+70+27"
         root.geometry(geometry)
-    htrack = 30
-    rmargin = 50
-    feat_pad = 10
     feat_thk = htrack - 2*feat_pad
     reg_bp = max(x[1] for t in tracks_content for x in t) # whole region size in bp
     reg_bp = max(reg_bp,nbp)
-    canvas = [tk.Canvas(root, height=htrack, bd=0, highlightthickness=0) for _ in names]
+    canvas = [tk.Canvas(root,height=htrack,bd=0,bg=canvas_bg,highlightthickness=0)
+              for _ in names]
     wlabel = 0
     # Draw labels
     for n,name in enumerate(names):
-        l = tk.Label(root,text=names[n],bd=0,highlightthickness=0)
+        l = tk.Label(root,text=names[n],bd=0,highlightthickness=0,bg=bg,padx=5)
         wlabel = max(l.winfo_reqwidth(),wlabel)
         l.grid(row=n,column=0)#,ipadx=4,ipady=4)
     wcanvas = WIN_WIDTH-wlabel-rmargin
     # Draw the tracks
     for n,t in enumerate(tracks_content):
-        format = os.path.splitext(names[n])[1]
-        if format.lower() in ['.bed']:
-            type = 'intervals'
-        elif format.lower() in ['.bedGraph','wig','.bigWig','.sga']:
-            type = 'density'
+        type = format(names[n])
         c = canvas[n]
         c.config(width=wcanvas)
         c.grid(row=n,column=1)
         if type == 'intervals':
-            c.create_line(0,htrack/2.,WIN_WIDTH,htrack/2.,fill="grey") # track axis
+            c.create_line(0,htrack/2.,WIN_WIDTH,htrack/2.,fill=line_col) # track axis
             y1,y2 = (0+feat_pad,feat_thk+feat_pad)
             for k,feat in enumerate(t):
-                x1,x2 = (feat[0],feat[1])
+                x1,x2,g = (feat[0],feat[1],feat[2])
                 x1 = _bp2px(x1,wcanvas,reg_bp)
                 x2 = _bp2px(x2,wcanvas,reg_bp)
-                c.create_rectangle(x1,y1,x2,y2, fill="blue")
+                c.create_rectangle(x1,y1,x2,y2, fill=feat_col)
         elif type == 'density':
-            pass
-        #c.create_line(0,0,0,htrack,fill="grey") # separator label|canvas
+            if t: top_bp = max(float(x[2]) for x in t) # highest score
+            c.create_line(0,htrack-1,WIN_WIDTH,htrack-1,fill=line_col) # track axis
+            for k,feat in enumerate(t):
+                x1,x2,s = (feat[0],feat[1],feat[2])
+                x1 = _bp2px(x1,wcanvas,reg_bp)
+                x2 = _bp2px(x2,wcanvas,reg_bp)
+                s = _bp2px(s,htrack,top_bp)
+                c.create_rectangle(x1,htrack-1,x2,htrack-s+5,fill=feat_col)
     # Add a blank frame on the right as a margin
     for n in range(len(names)):
-        w = tk.Frame(width=rmargin,height=htrack,bg="grey")
+        w = tk.Frame(width=rmargin,height=htrack,bg=bg)
         w.grid(row=n,column=2)
-    # Axis & ticks
+    # Axis
     minpos = maxpos
     if nbp: maxpos = ntimes*nbp
     elif nfeat: maxpos = reg_bp
-    min_label = tk.Label(text=str(minpos),bd=0,anchor='e')
-    min_label.grid(row=n+1,column=0,sticky='e',ipadx=5)
-    c = tk.Canvas(root,width=wcanvas,height=2*htrack,bd=0,highlightthickness=0)
+    min_label = tk.Label(text=str(minpos),bd=0,bg=bg,anchor='e')
+    min_label.grid(row=n+1,column=0,sticky='e',padx=5)
+    c = tk.Canvas(root,width=wcanvas,height=2*htrack,bd=0,bg=canvas_bg,highlightthickness=0)
     c.grid(row=n+1,column=1)
     pad = c.winfo_reqheight()/2.
-    #c.create_line(0,0,0,2*pad,fill="grey")         # separator
-    c.create_line(0,pad,WIN_WIDTH,pad) # axis
+    c.create_line(0,pad,WIN_WIDTH,pad,fill=line_col)  # axis
+    # Ticks & labels
     for n,t in enumerate(tracks_content):
         for k,feat in enumerate(t):
             f1,f2 = (feat[0],feat[1])
             x1 = _bp2px(f1,wcanvas,reg_bp)
             x2 = _bp2px(f2,wcanvas,reg_bp)
-            c.create_line(x1,pad,x1,pad-5)
-            c.create_line(x2,pad,x2,pad+5)
+            c.create_line(x1,pad,x1,pad-5,fill=line_col)
+            c.create_line(x2,pad,x2,pad+5,fill=line_col)
             if nbp:
                 f1 = feat[0]+(ntimes-1)*nbp
                 f2 = feat[1]+(ntimes-1)*nbp
-            print f1,f2,'-',minpos,maxpos
             if f1!=minpos and f1!=maxpos:
                 c.create_text(x1,pad-5,text=str(f1),anchor='s')
             if f2!=minpos and f2!=maxpos:
                 c.create_text(x2,pad+5,text=str(f2),anchor='n')
-    max_label = tk.Label(text=str(maxpos),bd=0,anchor='w')
-    max_label.grid(row=n+1,column=2,sticky='w')
+    max_label = tk.Label(text=str(maxpos),bd=0,bg=bg,anchor='w')
+    max_label.grid(row=n+1,column=2,sticky='w',padx=5)
     root.wm_attributes("-topmost", 1) # makes the window appear on top
     return root,maxpos
 
@@ -245,3 +260,5 @@ if __name__ == '__main__':
 #print 'Toyield', [[(x[0]+(k-1)*nbp,x[1]+(k-1)*nbp) for x in t] for t in toyield]
 #bg = "#%02x%02x%02x" % (255, 255, 224) #beige background
 #print "Window", str((k-1)*nbp)+'-'+str(k*nbp)
+        #c.create_line(0,0,0,htrack,fill="grey") # separator label|canvas
+    #c.create_line(0,0,0,2*pad,fill=line_col)         # separator
